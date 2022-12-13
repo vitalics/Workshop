c9eb290fffb3f947bf58c400e5ae8448212304f9
vitalics <vitalicset@yandex.ru>
2022-12-13 21:01:07 +0400

    Fix release script

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -2,24 +2,30 @@
 import { $, argv, fs, os } from "zx";
 
 const type = argv.type || argv.t;
-// for manual releases
 let msg = '';
 const upcomingFile = 'upcoming.md';
-
-const template = (type, msg) => `
----
-"@apps/app1": ${type}
-"@apps/e2e-app1-app2": ${type}
-"@utils/async: ${type}"
----
-${msg}
-`;
 
 function assertType(type) {
   if (!['major', 'minor', 'patch'].includes(type)) {
     throw new Error(`Invalid type: ${type}`);
   }
 }
+
+const template = (type, msg) => `
+---
+"@apps/e2e": ${type}
+"@apps/unifi": ${type}
+"@coxa/allure": ${type}
+"@coxa/env": ${type}
+"@coxa/logger": ${type}
+"@coxa/secrets": ${type}
+"@coxa/types": ${type}
+"@coxa/utils": ${type}
+"create-coxa-app": ${type}
+---
+
+${msg}
+`;
 
 assertType(type);
 
@@ -30,6 +36,8 @@ if (!fs.existsSync(upcomingFile)) {
 msg = await fs.readFile(upcomingFile, 'utf8');
 
 await $`chmod -R 777 .changeset`; // update permissions for execution
+await $`chmod -R 777 releases`; // update permissions for releases
+// await fs.copyFile(upcomingFile, `.changeset/${upcomingFile}`);
 
 // create empty changeset file
 await $`pnpm changeset --empty`;
@@ -41,11 +49,15 @@ await fs.writeFile(filename.trim(), template(type, msg)); // write changeset
 
 await $`pnpm changeset version`; // add changeset
 
+await $`pnpm install`; // install and resolve packages
+await $`pnpm add create-coxa-app -w`; // update workspace package
+
 const { stdout: version } = await $`pnpm version ${type} --no-git-tag-version`;
 
 // write to releases folder release.
 await fs.writeFile(`releases/${version.trim()}.md`, msg);
 // fill upcoming.md release with empty string
+console.log('cleanup upcoming file');
 await fs.writeFile(upcomingFile, os.EOL);
 
 const commitMsg = `${'Release ' + version.trim() + '\n\n' + msg}`;
@@ -54,12 +66,13 @@ const commitMsg = `${'Release ' + version.trim() + '\n\n' + msg}`;
 await $`git add .`;
 await $`git commit -m ${commitMsg}`;
 
+await $`pnpm changeset tag`;
+
 // create git tag without pushing
 await $`git tag -a ${version.trim()} -m ${commitMsg}`;
 
 // push tag
-await $`git push origin ${version.trim()} --tags -f`;
+await $`git push --follow-tags origin`;
 
-const { baseBranch } = await fs.readJSON('.changeset/config.json');
 // push main
-await $`git push -u origin ${baseBranch}`;
+await $`git push -u origin main`;
